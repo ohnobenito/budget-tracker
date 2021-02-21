b7528a1e49894669a4b51a425b43e8226db00a69
Benito Gonzalez <ohnobenito@gmail.com>
2021-02-21 14:18:45 -0600

    Updating to work offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,24 +24,6 @@ self.addEventListener("install", function (evt) {
   self.skipWaiting();
 });
 
-// activate
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
-
 // fetch
 self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/transaction")) {
